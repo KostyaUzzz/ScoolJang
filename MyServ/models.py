0e87338cd1acb6839e3fcf92ebebc8b86c11b639
Polina <polina2000_21@mail.ru>
2022-12-08 17:37:18 +0300

    was created model Product

--- MyServ/models.py
+++ MyServ/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(verbose_name='наименование товара', max_length=128, blank=False)
+    description = models.TextField(verbose_name='описание', max_length=254, blank=True)
+    photo = models.ImageField(upload_to='product_photo', blank=False, default='/unknowing/default.png')
+    category = models.CharField(verbose_name='категория товара/ тип', max_length=64, blank=False)
+    price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.product_name
--- MyServ/views.py
+++ MyServ/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.views.generic import ListView
+
+from MyServ.models import Product
+
+
 def show(request):
     return HttpResponse('Hello Django')
+
+
+class TitleContextMixin:
+
+    def get_title(self):
+        return getattr(self, 'title', '')
+
+    def get_context_data(self, **kwargs):
+        context = super(TitleContextMixin, self).get_context_data(**kwargs)
+        context.update(
+            title=self.get_title()
+
+        )
+        return context
+
+
+class Products(TitleContextMixin, ListView):
+    title = 'Продукты'
+    template_name = 'MyServ/index.html'
+    model = Product
